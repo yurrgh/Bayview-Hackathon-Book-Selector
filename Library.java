b23b1220d1bc4264c57269c58b419738d325a7b4
allheros2 <66750470+allheros2@users.noreply.github.com>
2022-07-22 17:25:29 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -27,5 +27,57 @@ public class Library
 		}
 		return complete;
 	}
+	public ArrayList<Book> getBooksWithTitle(String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < Books.size(); i++)
+		{
+			if(Books.get(i).getTitle() == s)
+			{
+				newBooks.add(Books.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithAuthor(String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < Books.size(); i++)
+		{
+			if(Books.get(i).getAuthor() == s)
+			{
+				newBooks.add(Books.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithGenre(String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < Books.size(); i++)
+		{
+			if(Books.get(i).getCategory() == s)
+			{
+				newBooks.add(Books.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithPages(int low, int high)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < Books.size(); i++)
+		{
+			if(Books.get(i).getPages() >= low && Books.get(i).getPages() <= high)
+			{
+				newBooks.add(Books.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooks()
+	{
+		return Books;
+	}
 	
 }
