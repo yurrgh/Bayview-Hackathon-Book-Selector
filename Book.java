f8209ade9139a331501a4a6d2d641a49db79f543
Joshua Beller <98232037+yurrgh@users.noreply.github.com>
2022-07-22 15:20:21 -0400

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,87 @@
+package hackathonBayview;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+
+public class Book {
+	String title, author, category, link, summary;
+	Double rating;
+	int pages;
+
+	public Book(String s) {
+		String temp=s;
+		String split=(temp.substring(temp.indexOf("[")+1,temp.indexOf("]") )) ;
+		title=split;
+
+		
+		for(int i=0;i<6;i++)
+		{
+			temp=temp.substring(  temp.indexOf("]")+1);
+			split=(temp.substring(temp.indexOf("[")+1,temp.indexOf("]") )) ;
+			switch(i)
+			{
+			   case 0: author=split;
+			   		break;
+				   	
+			   case 1: category=split;
+		   			break;  
+		   		
+			   case 2: pages=Integer.parseInt(split);
+		   			break;  
+		   		
+			   case 3: rating=Double.parseDouble(split);
+		   			break;
+		   			
+			   case 4: summary=split;
+	   				break;
+	   				
+			   case 5: link=split;
+  					break;
+		   			
+		   			
+				 
+			}
+		}
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	public String getCategory()
+	{
+		return category;
+	}
+	public int getPages()
+	{
+		return pages;
+	}
+	public Double getRating()
+	{
+		return rating;
+	}
+	public String getSummary()
+	{
+		return summary;
+	}
+	public String getLink()
+	{
+		return link;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
