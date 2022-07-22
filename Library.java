440bbd68ca4e8e9fd4dd332c8612caeaadbfc3ad
allheros2 <66750470+allheros2@users.noreply.github.com>
2022-07-22 16:54:49 -0400

    its library

---
+++ Library.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Library 
+{
+	ArrayList<Book> Books = new ArrayList<Book>();
+	public Library() throws FileNotFoundException
+	{
+		File fileText = new File("in.txt");
+		Scanner bookLine = new Scanner(fileText);
+		while(bookLine.hasNext())
+		{
+				String s = bookLine.nextLine();
+				Book book = new Book(s);
+				Books.add(book);
+		}
+	}
+	
+	public String completeLibraray()
+	{
+		String complete ="";
+		for(int i = 0; i < Books.size(); i++)
+		{
+			complete += Books.get(i).getTitle()+"\n";
+		}
+		return complete;
+	}
+	
+}
