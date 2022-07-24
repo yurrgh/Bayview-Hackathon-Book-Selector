a18d42674771280eac7e407d398e1e82748ce985
Joshua Beller <98232037+yurrgh@users.noreply.github.com>
2022-07-24 04:55:04 -0400

    Add files via upload

---
+++ FinalHackathon/Hackathon/src/hackathonBayview/Library.java
@@ -0,0 +1,103 @@
+package hackathonBayview;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Library 
+{
+	private ArrayList<Book> Books = new ArrayList<Book>();
+	public Library() throws FileNotFoundException
+	{
+		File fileText = new File("in.txt");
+		Scanner bookLine = new Scanner(fileText);
+		while(bookLine.hasNext())
+		{
+				String s = bookLine.nextLine();
+				Book book = new Book(s);
+				Books.add(book);
+		}
+	}
+	
+	public String completeLibrary()
+	{
+		String complete ="";
+		for(int i = 0; i < Books.size(); i++)
+		{
+			complete += Books.get(i).getTitle()+"\n";
+		}
+		return complete;
+	}
+	public ArrayList<Book> getBooksWithTitle(ArrayList<Book> selectedBooks, String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < selectedBooks.size(); i++)
+		{
+			if(selectedBooks.get(i).getTitle().toLowerCase().contains(s.toLowerCase()))
+			{
+				newBooks.add(selectedBooks.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithAuthor(ArrayList<Book> selectedBooks,String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < selectedBooks.size(); i++)
+		{
+			if(selectedBooks.get(i).getAuthor().toLowerCase().contains(s.toLowerCase()))
+			{
+				newBooks.add(selectedBooks.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithGenre(ArrayList<Book> selectedBooks,String s)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < selectedBooks.size(); i++)
+		{
+			if(selectedBooks.get(i).getCategory().toLowerCase().contains(s.toLowerCase()))
+			{
+				newBooks.add(selectedBooks.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithPages(ArrayList<Book> selectedBooks,int low, int high)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < selectedBooks.size(); i++)
+		{
+			if(Books.get(i).getPages() >= low && selectedBooks.get(i).getPages() <= high)
+			{
+				newBooks.add(selectedBooks.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getBooksWithRating(ArrayList<Book> selection,double rating)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		for(int i = 0; i < selection.size(); i++)
+		{
+			if(selection.get(i).getRating() >= rating)
+			{
+				newBooks.add(selection.get(i));
+			}
+		}
+		return newBooks;
+	}
+	public ArrayList<Book> getRandomBook(ArrayList<Book> selection)
+	{
+		ArrayList<Book> newBooks = new ArrayList<Book>();
+		newBooks.add(selection.get((int) (selection.size()*Math.random() ) ) );
+		return newBooks;
+	}
+	public ArrayList<Book> getBooks()
+	{
+		return Books;
+	}
+	
+}
---
+++ FinalHackathon/Hackathon/src/hackathonBayview/MainClass.java
@@ -0,0 +1,169 @@
+package hackathonBayview;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class MainClass {
+	
+		
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("		      Welcome to the Book Selector! \nStruggling to find a book? This program will find a book and get you hooked!\n");
+		boolean check = true;
+		while(check)
+		{
+		check = false;
+	    prompt();
+	    check = doYouWantToContinue();
+		}
+		System.out.print("\nWe hope we got you hooked on a book!");
+	}
+	
+	public static void prompt() throws FileNotFoundException
+	{
+		System.out.println("Please select filter");
+		System.out.println("1. Title \n2. Author \n3. Category/Genre \n4. Pages \n5. Rating \n6. Random Book\nPlease select your filter(s)");
+		Scanner sc= new Scanner(System.in);
+		String selected = sc.nextLine();
+			for(int s =1;s<=6;s++)
+			{
+				if(selected.contains(Integer.toString(s)))
+				{
+					selection(s);
+				}
+			}
+	}
+		
+	
+	
+	public static void selection(int choice) throws FileNotFoundException
+	{
+		Library library = new Library();
+	    ArrayList<Book> newBooks=library.getBooks();
+	    boolean check=true;
+		while(check)
+		{
+			switch(choice)
+			{
+				case 1: newBooks=library.getBooksWithTitle(newBooks,pTitle());
+					break;
+				case 2: newBooks=library.getBooksWithAuthor(newBooks,pAuthor());
+					break;
+				case 3: newBooks=library.getBooksWithGenre(newBooks,pGenre());
+					break;
+				case 4: newBooks=library.getBooksWithPages(newBooks,lPages(),hPages());
+					break;
+				case 5: newBooks=library.getBooksWithRating(newBooks,pRating());
+					break;
+				case 6: newBooks=library.getRandomBook(newBooks);
+						check=false;
+					break;
+			}
+			System.out.print("Do you wish to add more filters? " );
+		    Scanner sc= new Scanner(System.in);
+			String selected = sc.nextLine();
+			if(selected.equals("y"))
+			{
+				System.out.println("Please select another filter");
+				System.out.println("1. Title \n2. Author \n3. Category/Genre \n4. Pages \n5. Rating \nPlease select your filter");
+				Scanner redo= new Scanner(System.in);
+				choice = sc.nextInt();
+			}
+			else
+			{
+				check=false;
+			}
+			
+		}
+		System.out.print(newBooks);
+		
+	}
+	public static  String pTitle()
+	{
+		System.out.println("Please type in the titles name");
+		Scanner sc= new Scanner(System.in);
+		String selected = sc.nextLine();
+		return selected;
+		
+	}
+	public static String pAuthor()
+	{
+		System.out.println("Please type in the authors name");
+		Scanner sc= new Scanner(System.in);
+		String selected = sc.nextLine();
+		return selected;
+	}
+public static String pGenre()
+	{
+		System.out.println("1.Fantasy \n2.Sci-fi \n3.Dystopian\n4.Mystery \n5.Horror/Thriller \n6.Historical Fiction \n7.Short Story \n8.Manga \n9.Memoir and Autobiographies \n10.Self-Help \n11.How-To books \n12.Cultural Novels");
+		System.out.println("Please type in the number genre you want name");
+		Scanner sc= new Scanner(System.in);
+		String selected = sc.nextLine();
+		switch(Integer.parseInt(selected))
+		{
+			case 1: return "Fantasy";
+				
+			case 2: return "Sci-Fi";
+				
+			case 3: return "Dystopian";
+				
+			case 4: return "Mystery";
+			
+			case 5: return "Horror";
+			
+			case 6: return "Historical Fiction";
+			
+			case 7: return "Short Story";
+			
+			case 8: return "Manga";
+				
+			case 9: return "Memoir & Autobiography";
+			
+			case 10: return "Self-Help";
+			
+			case 11: return "How-To";
+		
+			case 12: return "Cultural";
+			
+			default: return "Fantasy";//Just chose a random one and it is the first one
+		}
+	}
+	
+	public static int hPages()
+	{
+		System.out.println("What is the maximum amount of pages?");
+		Scanner sc= new Scanner(System.in);
+		int high = sc.nextInt();
+		return high;
+	}
+	public static int lPages()
+	{
+		System.out.println("What is the minumum amount of pages?");
+		Scanner sc= new Scanner(System.in);
+		int low = sc.nextInt();
+		return low;
+	}
+	public static double pRating()
+	{
+		System.out.println("What is the lowest rating?");
+		Scanner sc= new Scanner(System.in);
+		double rate = sc.nextDouble();
+		return rate;
+	}
+	
+	public static boolean doYouWantToContinue()
+	{
+		System.out.println("\nDo you wish to find another book");
+		Scanner sc= new Scanner(System.in);
+		String decision = (sc.nextLine());
+		if(decision.equals("y")||decision.equals("yes"))
+		{return true;}
+		else 
+		{return false;}
+		
+	}
+
+	
+
+}
